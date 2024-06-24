ec557e7f28be055b3e0d879050b8e7e2876d0921
Lei <gabriel_sl19798@hotmail.com>
2024-06-24 18:41:54 +1200

    hope it works

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import HelloWorld from "./components/HelloWorld";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('renders correctly', () => {
+const component = renderer.create(<HelloWorld />);
+const tree = component.toJSON();
+expect(tree).toMatchSnapshot();
 });
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function HelloWorld(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default HelloWorld;
